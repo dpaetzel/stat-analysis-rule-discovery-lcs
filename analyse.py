490a96d2c9d15ed10eea30df8d4348d68d3b437c
David Pätzel <david.paetzel@posteo.de>
2022-07-01 17:39:16 +0200

    📄 Add license

--- analyse.py
+++ analyse.py
@@ -1,3 +1,20 @@
+# Script to perform the analysis for the 2022 paper "Approaches for Rule
+# Discovery in a Learning Classifier System" by Heider et al.
+#
+# Copyright (C) 2022 David Pätzel <david.paetzel@posteo.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import subprocess
 import sys
 from functools import partial
